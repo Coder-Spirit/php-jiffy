10319a1da141674cb34bdaa4c86dda78cecdebab
Andreu Correa Casablanca <castarco@gmail.com>
2016-03-05 18:02:13 +0100

    Moved two tests

--- tests/UniversalTimestampTests.php
+++ tests/UniversalTimestampTests.php
@@ -28,21 +28,6 @@ class UniversalTimestampTests extends \PHPUnit_Framework_TestCase
         $this->assertGreaterThanOrEqual($uTs_A->asSeconds(), $classicTs_B);
     }
 
-    public function testFromMongoDate()
-    {
-        if (!extension_loaded('mongo')) return;
-
-        $uTs_A = UniversalTimestamp::now();
-        $uTs_B = UniversalTimestamp::fromMongoDate(new \MongoDate());
-        $uTs_C = UniversalTimestamp::now();
-
-        $this->assertGreaterThanOrEqual(4, 5);
-
-        $this->assertGreaterThanOrEqual($uTs_A->asMilliseconds(), $uTs_B->asMilliseconds());
-        $this->assertGreaterThanOrEqual($uTs_B->asMilliseconds(), $uTs_C->asMilliseconds());
-        $this->assertGreaterThanOrEqual($uTs_A->asMilliseconds(), $uTs_C->asMilliseconds());
-    }
-
     public function testFromSeconds()
     {
         $classicTs_A = time();
@@ -151,18 +136,6 @@ class UniversalTimestampTests extends \PHPUnit_Framework_TestCase
         if ($ts1->asMilliseconds() === $ts2->asMilliseconds()) {
             $this->assertTrue($ts2->getRemainingMicroseconds() > $ts1->getRemainingMicroseconds());
         }
-    }
-
-    public function testAsMongoDate()
-    {
-        if (!extension_loaded('mongo')) return;
-
-        $ts1 = UniversalTimestamp::fromMongoDate(new \MongoDate());
-        $md1 = $ts1->asMongoDate();
-
-        $this->assertTrue($md1 instanceof \MongoDate);
-        $this->assertEquals($ts1->asSeconds(), $md1->sec);
-        $this->assertEquals($ts1->asMilliseconds()%1000, $md1->usec/1000);
     }
 
     public function testToString()
